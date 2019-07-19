c46645709304bef876a75f82b097adf8b68472c9
André Gomes <andre.gomes@bee-eng.pt>
2019-07-19 17:35:01 +0100

    Corrigindo

--- HtmlDynamicLibrary/CustomTagBuilders/TagBuilder_Select.cs
+++ HtmlDynamicLibrary/CustomTagBuilders/TagBuilder_Select.cs
@@ -60,8 +60,6 @@ namespace HtmlDynamicLibrary.CustomTagBuilders
 			if (this.optionLabel != null)
 			{
 				option = new TagBuilder("option");
-				if (this.optionLabel.Value.ToString().Trim() == this.Value.ToString().Trim())
-					option.MergeAttribute("selected", "true");
 				option.MergeAttribute("value", this.optionLabel.Value.ToString());
 				option.SetInnerText(this.optionLabel.Text);
 				options += option.ToString(TagRenderMode.Normal) + "\n";
