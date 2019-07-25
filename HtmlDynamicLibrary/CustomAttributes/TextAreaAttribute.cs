11cf5894e3e52be1f832b4e685c54d051e0f7873
André Gomes <andre.gomes@bee-eng.pt>
2019-07-25 10:28:22 +0100

    Inclusão de novos atributos

--- HtmlDynamicLibrary/CustomAttributes/TextAreaAttribute.cs
+++ HtmlDynamicLibrary/CustomAttributes/TextAreaAttribute.cs
@@ -6,21 +6,18 @@ namespace System.ComponentModel.DataAnnotations
 	public class TextAreaAttribute : DynamicAttribute
 	{
 		public int Rows { get; private set; }
-		public int? Cols { get; private set; }
-		public bool HardWrap { get; private set; }
-		public int? MinLength { get; private set; }
-		public int? MaxLength { get; private set; }
+		public int Cols { get; private set; }
+		public bool HardWrap { get; set; }
+		public int MinLength { get; set; }
+		public int MaxLength { get; set; }
 
-		public TextAreaAttribute(int rows, int cols = -1, int minLength = -1, int maxLength = -1, bool hardWrap = false)
+		public TextAreaAttribute(int rows, int cols = -1)
 			: base()
 		{
 			DataTypeAttribute dataType = new DataTypeAttribute(DataType.MultilineText);
 
 			this.Rows = rows;
-			this.Cols = cols >= 0 ? cols : (int?)null;
-			this.HardWrap = hardWrap;
-			this.MinLength = minLength >= 0 ? minLength : (int?)null;
-			this.MaxLength = maxLength >= 0 ? maxLength : (int?)null;
+			this.Cols = cols;
 		}
 	}
 }
